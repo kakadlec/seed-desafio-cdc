3e3d28e31f87091570877538a02d4632f5fedbf7
Karel Kadlec <kakadlec@gmail.com>
2024-11-24 20:06:15 -0300

    Adding breeze API

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('phpinfo', function () {
-    phpinfo();
-})->name('phpinfo');
+require __DIR__.'/auth.php';
